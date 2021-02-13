48abb149c7570f1bca99f55a36ea54fccce2ba0e
iMoto251 <dbrider251@gmail.com>
2021-02-12 18:54:16 -0600

    Kind of works, working on more

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,41 +1,49 @@
 import math;
 import string;
+longwords = []
 
-def main():
-
+def getwords():
     file1 = open('hw1Input.txt', 'r')
     Lines = file1.readlines()
-
-    words = []
-    longwords = []
-    #punc = '''!()-[]{};:'"\,= <>./?@#$%^&*_~'''
-    punc = string.punctuation
 
 
     for line in Lines:
         if line.strip():
-            for ele in line:
-                if ele in punc:
-                    line = line.replace(ele," ")
+            for a in line:
+                if a in string.punctuation:
+                    line = line.replace(a," ")
             line = line.split()
             longwords.append(sorted(line, key=len))
         else:
             '''do nothing'''
-    
-    #print(longwords)
-    
+    file1.close()
+    return longwords
+
+def writedictionary(longwords):
+    file2 = open('hw1Output.txt', 'w')
+    sorteddict = []
     for x in range(len(longwords)):
         try:
-            print (longwords[x][-1].lower())
-            print (longwords[x][-2].lower())
-            print (longwords[x][-3].lower())
-            print (longwords[x][-4].lower())
-            print (longwords[x][-5].lower())
-            print (longwords[x][-6].lower())
+            sorteddict.append((longwords[x][-1].lower() + ": " + longwords[x][-2].lower() + ", " + longwords[x][-3].lower() + ", " + longwords[x][-4].lower() + ", " + longwords[x][-5].lower() + ", " + longwords[x][-6].lower()))
         except IndexError:
             pass
-        print(" ---------- ")
         continue
+
+    sorteddict.sort()
+    #print sorteddict
+    for y in range(len(sorteddict)):
+        try:
+            file2.write(sorteddict[y] + "\n")
+        except IndexError:
+            pass
+        continue
+
+    file2.close()
+
+
+def main():
+    thedict = getwords()
+    writedictionary(thedict)
 
 if __name__ == '__main__':
     import sys;
